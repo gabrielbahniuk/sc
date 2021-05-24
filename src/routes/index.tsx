f3d473fbd6496e5f755200386e768a6d335fc80c
Gabriel <hi@thegabriel.dev>
2021-05-24 18:19:56 +0200

    feat: add routes for initial investment and result pages, also redirect when wrong url

---
+++ src/routes/index.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import React from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
+import InitialInvestment from '@/pages/InitialInvestment'
+import Result from '@/pages/Result'
+import Header from '@/components/Header'
+
+const Routes: React.FC = () => {
+  return (
+    <>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={InitialInvestment} />
+        <Route exact path="/result" component={Result} />
+        <Route render={() => <Redirect to="/" />} />
+      </Switch>
+    </>
+  )
+}
+
+export default Routes
