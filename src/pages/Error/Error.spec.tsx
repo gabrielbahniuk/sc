ab46cb059d12933541f35e167b300a5e52a52c29
Gabriel <hi@thegabriel.dev>
2021-05-24 18:20:57 +0200

    test: create tests for Error page

---
+++ src/pages/Error/Error.spec.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { shallow, ShallowWrapper } from 'enzyme'
+import { create } from 'react-test-renderer'
+import Error from '.'
+
+let wrapper: ShallowWrapper
+
+beforeAll(() => {
+  wrapper = shallow(<Error />)
+})
+
+afterAll(() => {
+  wrapper.unmount()
+})
+
+describe('Error Component', () => {
+  it('should render Error component', () => {
+    const errorComponent = create(<Error />).toJSON()
+    expect(errorComponent).toMatchSnapshot()
+  })
+
+  it('should render title element', () => {
+    expect(wrapper.find('h1').length).toBe(1)
+  })
+  it('should render button', () => {
+    expect(wrapper.find('Button').length).toBe(1)
+  })
+})
