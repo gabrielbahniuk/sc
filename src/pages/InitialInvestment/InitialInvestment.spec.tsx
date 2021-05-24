ae5798ada374128520b726d9da5af4e1b9c86ce4
Gabriel <hi@thegabriel.dev>
2021-05-24 18:30:47 +0200

    test: create tests for Initial Investment page

---
+++ src/pages/InitialInvestment/InitialInvestment.spec.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { shallow, ShallowWrapper } from 'enzyme'
+import InitialInvestment from '.'
+import Form from '@/components/Form'
+import { create } from 'react-test-renderer'
+
+describe('InitialInvestment Page', () => {
+  it('should match snapshot', () => {
+    const initialInvestmentPage = create(<InitialInvestment />).toJSON()
+    expect(initialInvestmentPage).toMatchSnapshot()
+  })
+  it('should render page components', () => {
+    const wrapper: ShallowWrapper = shallow(<InitialInvestment />)
+    expect(wrapper.find('h1').length).toEqual(1)
+    expect(wrapper.find(Form).length).toEqual(1)
+  })
+})
