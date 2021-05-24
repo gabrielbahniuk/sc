65b27552aaffb4a63bf5c12b24c29ddecaf38ac0
Gabriel <hi@thegabriel.dev>
2021-05-24 18:43:08 +0200

    test: create tests for Header component

---
+++ src/components/Header/Header.spec.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { mount, ReactWrapper } from 'enzyme'
+import Header from '.'
+
+describe('Header Component', () => {
+  it('should render logo within header', () => {
+    const wrapper: ReactWrapper = mount(<Header />)
+    expect(wrapper.find('svg').length).toEqual(1)
+    wrapper.unmount()
+  })
+})
