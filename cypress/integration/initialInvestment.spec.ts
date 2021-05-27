0fb7e43788705cb274dabc5ca83ae1844c87d915
Gabriel <hi@thegabriel.dev>
2021-05-27 23:16:53 +0200

    test: add new test to load page with default values

--- cypress/integration/initialInvestment.spec.ts
+++ cypress/integration/initialInvestment.spec.ts
@@ -4,11 +4,24 @@ context('Initial Investments page', () => {
     riskLevel: '3',
     yearsInvestment: '1'
   }
+  const newValues = {
+    investmentValue: '11000',
+    riskLevel: '4',
+    yearsInvestment: '2'
+  }
+  const url = 'http://localhost:3000'
 
   it('should load page with default values', () => {
+    cy.visit(url)
     cy.loadInitialValues(initialValues)
   })
+  it('should be able to change default values', () => {
+    cy.visit(url)
+    cy.loadInitialValues(initialValues)
+    cy.setNewValues(newValues)
+  })
   it('should set correct progress bar color on risk level change', () => {
+    cy.visit(url)
     cy.loadInitialValues(initialValues)
     cy.get('[data-cy=riskLevelSelector]').select('5')
     cy.get('[data-cy=progressBar]').should('have.css', 'background-color', 'rgb(0, 128, 0)')
