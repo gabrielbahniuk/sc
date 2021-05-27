9c519fb1c3c4bfeee29d6325d2fc664777e519aa
Gabriel <hi@thegabriel.dev>
2021-05-27 23:17:51 +0200

    test: render a loading spinner while api call is loading data

--- cypress/integration/result.spec.ts
+++ cypress/integration/result.spec.ts
@@ -4,27 +4,22 @@ context('Result page', () => {
     riskLevel: '3',
     yearsInvestment: '1'
   }
+  const url = 'http://localhost:3000'
 
-  const newValues = {
-    investmentValue: '11000',
-    riskLevel: '4',
-    yearsInvestment: '2'
-  }
-
-  it('should render Error component if empty cones array', () => {
+  it('should render Loading spinner while cones array is empty', () => {
+    cy.intercept('GET', '**/api/cones', { fixture: 'cones/empty.json' })
+    cy.visit(url)
     cy.loadInitialValues(initialValues)
-    cy.setNewValues(newValues)
-    cy.intercept('**/api/cones', { fixture : 'cones/empty.json'})
     cy.get('[data-cy=btnCalculate]').click()
-    cy.get('[data-cy=titleError]').should('have.length', 1)
-    cy.get('[data-cy=btnReset]').should('have.length', 1)
+    cy.get('[data-cy=loadingComponent]').should('have.length', 1)
   })
 
   it('should calculate investments with cones array', () => {
-    cy.loadInitialValues(initialValues)
-    cy.setNewValues(newValues)
     cy.intercept('GET', '**/api/cones', { fixture: 'cones/cones.json' })
+    cy.visit(url)
+    cy.loadInitialValues(initialValues)
     cy.get('[data-cy=btnCalculate]').click()
+    cy.get('[data-cy=resultContainer]').should('have.length', 1)
     cy.get('[data-cy=titleResult]').should('have.length', 1)
     cy.get('[data-cy=btnReset]').should('have.length', 1)
     cy.get('[data-cy=btnToggleTable]').should('have.length', 1)
